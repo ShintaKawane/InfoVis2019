e4ba8a9cc325f8321db9e096245bfe8c344fb37e
河根　慎太 <1675080t@istc-a-068.epc.center.kobe-u.ac.jp>
2019-05-21 14:51:41 +0900

    0521

---
+++ W09/main_task01.js
@@ -0,0 +1,32 @@
+function main()
+{
+    var volume = new KVS.LobsterData();
+    var screen = new KVS.THREEScreen();
+
+    screen.init( volume, {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        enableAutoResize: false
+    });
+
+    var bounds = Bounds( volume );
+    screen.scene.add( bounds );
+
+    var isovalue = 10;
+    var surfaces = Isosurfaces( volume, isovalue );
+    screen.scene.add( surfaces );
+
+    var isovalue2 = 200;
+    var surfaces2 = Isosurfaces( volume, isovalue2 );
+    screen.scene.add( surfaces2 );
+
+    document.addEventListener( 'mousemove', function() {
+        screen.light.position.copy( screen.camera.position );
+    });
+
+    window.addEventListener( 'resize', function() {
+        screen.resize( [ window.innerWidth, window.innerHeight ] );
+    });
+
+    screen.loop();
+}
